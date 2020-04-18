275e9bd6b46465d2f8560b786326da3df84b9121
jacobguinther <jacobgguinther@gmail.com>
2020-04-18 16:39:29 -0600

    add rating schema

---
+++ models/rating.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const ratingSchema = mongoose.Schema({
+    rating: Number,
+    createdAt: { type: Date, default: Date.now },
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
+});
+
+module.exports = mongoose.model("Rating", ratingSchema);
