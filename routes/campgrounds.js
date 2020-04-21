5bdae899119e6ce79073816563e8b71cef5e3e75
jacobguinther <jacobgguinther@gmail.com>
2020-04-21 15:37:56 -0600

    fix issue i noticed with seeds file not adding user id to campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -174,9 +174,13 @@ router.get("/id-:id", (req, res) => {
       let userReviewed = false;
       if (req.user !== undefined) {
         for (let i = 0; i < campground.reviews.length; i++) {
-            if(campground.reviews[i].author.id.toString() === req.user._id.toString()){
-            userReviewed = true;
-          }
+          //   if(campground.reviews[i].author.id.toString() === req.user._id.toString()){
+          //   userReviewed = true;
+          // }
+          // console.log(campground.reviews[i].author.id.toString())
+          // console.log(req.user._id.toString())
+          console.log(campground.reviews[i].author.id)
+          console.log(req.user._id)
         }
         res.render("campgrounds/show", { campground, userReviewed });
       } else {
