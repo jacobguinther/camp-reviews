491672bf8c9b7e66a62e7204fa14a68b9e624538
jacobguinther <jacobgguinther@gmail.com>
2020-04-28 10:06:53 -0600

    lind routes/reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,22 @@
-const express = require("express"),
-  router = express.Router({ mergeParams: true }),
-  Campground = require("../models/campground"),
-  Review = require("../models/review"),
-  middleware = require("../middleware"),
-  { isLoggedIn, checkUserReview, checkIfUserReviewed } = middleware;
+const express = require('express');
+const Review = require('../models/review');
+const middleware = require('../middleware');
+const Campground = require('../models/campground');
+
+const router = express.Router({ mergeParams: true });
+const { isLoggedIn, checkUserReview, checkIfUserReviewed } = middleware;
 
 // CREATE REVIEW PAGE
-router.get("/new", isLoggedIn, checkIfUserReviewed, (req, res) => {
-  res.render("reviews/new", { campground: req.campground });
+router.get('/new', isLoggedIn, checkIfUserReviewed, (req, res) => {
+  res.render('reviews/new', { campground: req.campground });
 });
 
 // CREATE REVIEW
-router.post("/", isLoggedIn, checkIfUserReviewed, (req, res) => {
-  console.log("CREATE REVIEW ROUTE HIT");
+router.post('/', isLoggedIn, checkIfUserReviewed, (req, res) => {
+  console.log('CREATE REVIEW ROUTE HIT');
   const { comment, rating } = req.body;
   const { _id: id, username } = req.user;
-  let review = { comment, rating };
+  const review = { comment, rating };
   const CAMPGROUND = Campground.findById(req.params.id);
   const REVIEW = Review.create(review);
   const promiseArr = [CAMPGROUND, REVIEW];
@@ -29,40 +30,40 @@ router.post("/", isLoggedIn, checkIfUserReviewed, (req, res) => {
       review.save();
       campground.reviews.push(review);
       campground.save();
-      req.flash("success", "Created a review!");
-      res.redirect("/campgrounds/" + campground._id);
+      req.flash('success', 'Created a review!');
+      res.redirect(`/campgrounds/${campground._id}`);
     })
     .catch((err) => {
-      console.log("ERROR CREATING REVIEW:", err);
-      req.flash("error", "Error creating review.");
-      res.redirect("/campgrounds/" + req.params.id);
+      console.log('ERROR CREATING REVIEW:', err);
+      req.flash('error', 'Error creating review.');
+      res.redirect(`/campgrounds/${req.params.id}`);
     });
 });
 
 // UPDATE REVIEW PAGE
-router.get("/:reviewId/edit", isLoggedIn, checkUserReview, (req, res) => {
-  res.render("reviews/edit", {
+router.get('/:reviewId/edit', isLoggedIn, checkUserReview, (req, res) => {
+  res.render('reviews/edit', {
     campground_id: req.params.id,
     review: req.review,
   });
 });
 
 // UPDATE REVIEW
-router.put("/:reviewId", isLoggedIn, checkUserReview, (req, res) => {
-  console.log("UPDATE REVIEW ROUTE HIT");
+router.put('/:reviewId', isLoggedIn, checkUserReview, (req, res) => {
+  console.log('UPDATE REVIEW ROUTE HIT');
   const { comment, rating } = req.body;
-  let review = { comment, rating };
+  const review = { comment, rating };
   const REVIEW = Review.findByIdAndUpdate(req.params.reviewId, review);
   REVIEW.then(() => {
-    res.redirect("/campgrounds/" + req.params.id);
+    res.redirect(`/campgrounds/${req.params.id}`);
   }).catch((err) => {
-    console.log("ERROR UPDATING REVIEW", err);
-    res.render("edit");
+    console.log('ERROR UPDATING REVIEW', err);
+    res.render('edit');
   });
 });
 
 // DELETE REVIEW
-router.delete("/:reviewId", isLoggedIn, checkUserReview, (req, res) => {
+router.delete('/:reviewId', isLoggedIn, checkUserReview, (req, res) => {
   Campground.findByIdAndUpdate(
     req.params.id,
     {
@@ -72,20 +73,20 @@ router.delete("/:reviewId", isLoggedIn, checkUserReview, (req, res) => {
     },
     (err) => {
       if (err) {
-        console.log("ERROR DELETING REVIEW:", err);
-        req.flash("error", err.message);
-        res.redirect("/");
+        console.log('ERROR DELETING REVIEW:', err);
+        req.flash('error', err.message);
+        res.redirect('/');
       } else {
         req.review.remove((err) => {
           if (err) {
-            req.flash("error", err.message);
-            return res.redirect("/");
+            req.flash('error', err.message);
+            return res.redirect('/');
           }
-          req.flash("error", "Review deleted!");
-          res.redirect("/campgrounds/" + req.params.id);
+          req.flash('error', 'Review deleted!');
+          return res.redirect(`/campgrounds/${req.params.id}`);
         });
       }
-    }
+    },
   );
 });
 
